d01a040a7ef020cb1303e0b47616019ea57e92fb
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-05-28 12:45:23 -0400

    initial file structure for dev

---
+++ src/components/nav.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export const NavMobile = () => <div>Menu Bar for Mobile</div>
+
+export const NavDesktop = () => <div>Menu Bar for Desktop</div>
---
+++ src/components/project.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./project.module.css"
+
+const Project = props => <div>Project Tile</div>
+
+export default Project
