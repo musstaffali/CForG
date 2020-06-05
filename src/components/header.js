15f3c179304808846b1728fd56a511a84ba6f4f1
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-06-05 12:58:37 -0400

    mobile nav roughed in

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,10 @@
-import PropTypes from "prop-types"
 import React from "react"
-import styles from "./header.module.css"
+import Nav from "./nav"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0, color: `white`, textDecoration: `none` }}>
-        {siteTitle}
-      </h1>
-    </div>
+  <header>
+    <Nav />
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
