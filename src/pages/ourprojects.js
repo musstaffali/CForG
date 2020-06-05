6440f5eed4c84fb0e91d5bee8233d51eb346e1b8
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-06-05 11:18:38 -0400

    bulma and sass init install

---
+++ src/pages/ourprojects.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const OurProjects = () => (
+  <Layout>
+    <h1>Our Projects Page</h1>
+  </Layout>
+)
+
+export default OurProjects
