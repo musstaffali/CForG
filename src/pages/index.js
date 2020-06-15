5f1bd4d02d142dadc4ef3a0730a95e99f650e463
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-06-15 14:01:35 -0400

    homepage roughed in... hero css responsive needs work

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,28 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Code for Greensboro - Local Code for America Brigade" />
-    <h1>Hello Greensboro!</h1>
-    <p>
-      Shiny new website for Code for Greensboro - the Greensboro, NC Code for
-      America brigade
-    </p>
-    <p>Coming Soon to a browser near you!</p>
-    <a href="https://codeforgsogithubio.codeforgso.now.sh/">
-      The Previous Site
-    </a>
+    <section className="hero is-success is-halfheight has-hero-img">
+      <div className="hero-body">
+        <h1 className="title hero-mission">
+          Code for Greensboro's mission is to engage our community and its
+          talent through civic action, open data, and technology
+        </h1>
+      </div>
+    </section>
+    <section className="hero-about">
+      <div className="container">
+        <h1>WHO WE ARE</h1>
+        <p>
+          Code for Greensboro is a brigade from a larger organization called
+          Code for America. It is our vision and mission to improve the
+          well-being of our communities in Greensboro through utilizing
+          technology, process, and (most importantly) the people. This is for us
+          and operated by us. If you’re a coder, designer, or have something
+          else to contribute, we’d love to have you come along with us on this
+          journey.{" "}
+        </p>
+      </div>
+    </section>
   </Layout>
 )
 
