99dfb917baf26a6c39ee6cda59a74639eb76a40b
Tr3yb0 <trey.botard@gmail.com>
2020-08-10 22:28:14 -0400

    Added margin to button labels on join page.
    Changed button from h1 to h2 and added a hover effect

--- src/pages/join.js
+++ src/pages/join.js
@@ -3,29 +3,29 @@ import Layout from "../components/layout"
 
 const Join = () => (
   <Layout>
-    <h1 className="mt-6">Slack</h1>
+    <h1 className="mt-6 join-label">Slack</h1>
     <a href="https://slack.codeforgreensboro.org/">
       <div className="columns is-mobile is-gapless mt-3">
-        <div className="column is-four-fifths join-title has-background-danger">
-          <h1>Connect</h1>
+        <div className="column is-four-fifths join-title">
+          <h2>Connect</h2>
         </div>
         <div className="column has-background-primary"></div>
       </div>
     </a>
-    <h1 className="mt-6">GitHub</h1>
+    <h1 className="mt-6 join-label">GitHub</h1>
     <a href="https://github.com/codeforgso">
       <div className="columns is-mobile is-gapless mt-3">
         <div className="column is-four-fifths join-title">
-          <h1>Work With Us</h1>
+          <h2>Work With Us</h2>
         </div>
         <div className="column has-background-primary"></div>
       </div>
     </a>
-    <h1 className="mt-6">MeetUp</h1>
+    <h1 className="mt-6 join-label">MeetUp</h1>
     <a href="https://www.meetup.com/Code-for-Greensboro/">
       <div className="columns is-mobile is-gapless mt-3">
         <div className="column is-four-fifths join-title">
-          <h1>Meet Us</h1>
+          <h2>Meet Us</h2>
         </div>
         <div className="column has-background-primary"></div>
       </div>
