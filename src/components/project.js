d4a024926235cc4ab82efde46ed9eefbaf1151fe
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-08-15 15:14:04 -0400

    WIP projects page

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
 import styles from "./project.module.css"
-
-const Project = props => <div>Project Tile</div>
-
+import OvalButton from "./oval_button.js"
+function Project(props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>{props.title}</h1>
+      </div>
+      <section>
+        <div className={styles.status}>{props.status}</div>
+        <a href={props.repo} target="blank" rel="noreferrer">
+          <h2 className={styles.ghlink}>GitHub</h2>
+        </a>
+        <p className={styles.body}>{props.body}</p>
+      </section>
+      <OvalButton text="Join Project" external="true" link={props.slack} />
+    </div>
+  )
+}
 export default Project
--- src/pages/ourprojects.js
+++ src/pages/ourprojects.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
+import Project from "../components/project.js"
 
 const OurProjects = () => (
   <Layout>
-    <h1>COMING SOON - Our Projects Page</h1>
+    <Project
+      title="Go Vote App"
+      status="Active"
+      repo="https://github.com/codeforgso/GoVote"
+      body="We see a need for an app that helps people become voters with less barriers such as lack of education, transportation, and bias systems."
+      slack="https://cfgso.slack.com/messages/proj-govotegso"
+    />
   </Layout>
 )
 
