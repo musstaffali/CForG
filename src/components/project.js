1d16def8998c21f4503944783d204541e3447a5c
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-08-18 20:09:09 -0400

    projects page edits

--- src/components/project.js
+++ src/components/project.js
@@ -2,20 +2,30 @@ import React from "react"
 import styles from "./project.module.scss"
 
 function Project(props) {
+  var footeritem
+  if (props.link != "") {
+    footeritem = (
+      <a
+        className="card-footer-item dkblue"
+        href={props.link}
+        target="blank"
+        rel="noreferrer"
+      >
+        {props.linktxt}
+      </a>
+    )
+  } else footeritem = <p className="card-footer-item dkblue">{props.linktxt}</p>
+
   return (
     <div className="card my-5">
       <div className="card-header">
         <h2 className="card-header-title">{props.title}</h2>
       </div>
-        <div className={styles.status}>
-          Status: {props.status}
-        </div>
-        <div className="card-content">
+      <div className={styles.status}>Status: {props.status}</div>
+      <div className="card-content">
         <p className={styles.body}>{props.body}</p>
-        </div>
-        <div className="card-footer">
-          <a className="card-footer-item dkblue" href={props.repo} target="blank" rel="noreferrer">Contribute Now</a>
-        </div>
+      </div>
+      <div className="card-footer">{footeritem}</div>
     </div>
   )
 }
